3507fb50ce2256ef1e7fd1c6beafe9f7b3f93640
Jasmine Jisu Yu <jasmine.jisu@icloud.com>
2026-04-22 17:49:13 +0800

    update: mongoDB connection pooling

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 
+// eslint-disable-next-line prefer-const
+let isConnected = false;
+
 export default async function mongo() {
+  if (isConnected) return;
+
   const MONGO = process.env.MONGO_URI;
   if (!MONGO) {
     throw new Error("Mongo is not defined");
